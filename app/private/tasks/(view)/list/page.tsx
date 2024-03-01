f19c80ef2d412d617e13a8fa618e0ca3deeaee3b
MD Mehedi Hasan <mehedihasan.hr.324@gmail.com>
2024-03-01 11:43:47 +0600

    update'

--- app/private/tasks/(view)/list/page.tsx
+++ app/private/tasks/(view)/list/page.tsx
@@ -1,18 +1,23 @@
 import React from "react";
 import AddTaskButtonWithDropdown from "@/components/button/add-task-button-with-dropdown";
 import TaskDataTable from "@/components/tables/tasks/data-table";
-// import { config } from "@/config";
+import { config } from "@/config";
 
-// const fetchTasks = async () => {
-//   const tasks = await fetch(`${config.API}/tasks`, { cache: "no-cache" }).then(
-//     (res) => res.json()
-//   );
-//   return tasks;
-// };
+const fetchTasks = async () => {
+  try {
+    const tasks = await fetch(`${config.API}/tasks`, {
+      cache: "no-cache",
+    }).then((res) => res.json());
+
+    return tasks;
+  } catch (error) {
+    console.log({ error });
+    throw Error("error");
+  }
+};
 
 async function TaskListView() {
-  // const { tasks } = await fetchTasks();
-  const tasks: never[] = [];
+  const { tasks } = await fetchTasks();
 
   return (
     <>
